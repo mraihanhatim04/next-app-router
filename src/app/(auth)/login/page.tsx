7cc56d14316641cff1ded6574c73ac2de925f7fe
mraihanhatim <raihan04042001@gmail.com>
2024-10-18 12:11:03 +0700

    create login & register form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,72 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 export default function LoginPage() {
   return (
     <>
-      <div className="flex h-[561px] justify-center items-center">
-        <h1 className="tracking-wider font-semibold">Login Page</h1>
+      <div className="flex h-screen justify-center items-center p-4">
+        <Card className="w-[450px] shadow-xl">
+          <CardHeader>
+            <CardTitle className="text-3xl font-bold">Login</CardTitle>
+            <CardDescription className="font-semibold text-slate-600 tracking-widest">
+              Sign In to our platform
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1">
+                  <Label htmlFor="username">Username</Label>
+                  <Input
+                    type="text"
+                    id="username"
+                    placeholder="Username"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    placeholder="Example: email@gmail.com"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    type="password"
+                    id="password"
+                    placeholder="**********"
+                    required
+                  />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-3">
+            <Button className="w-full">Login</Button>
+            <p className="text-sm">
+              Not Registered?{" "}
+              <Link href="/register">
+                <span className="underline font-bold">Create Account</span>
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
       </div>
     </>
   );
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,8 +1,81 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 export default function RegisterPage() {
   return (
     <>
-      <div className="flex h-[561px] justify-center items-center">
-        <h1 className="tracking-wider font-semibold">Register Page</h1>
+      <div className="flex h-screen justify-center items-center p-4">
+        <Card className="w-[450px] shadow-xl">
+          <CardHeader>
+            <CardTitle className="text-3xl font-bold">Register</CardTitle>
+            <CardDescription className="font-semibold text-slate-600">
+              Sign Up to our platform
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1">
+                  <Label htmlFor="fullname">FullName</Label>
+                  <Input
+                    type="text"
+                    id="fullname"
+                    placeholder="Your FullName"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <Label htmlFor="username">Username</Label>
+                  <Input
+                    type="text"
+                    id="username"
+                    placeholder="Your Username"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    placeholder="Example: Email@gmail.com"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    type="password"
+                    id="password"
+                    placeholder="**********"
+                    required
+                  />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-3">
+            <Button className="w-full">Register</Button>
+            <p className="text-sm">
+              Have an account?{" "}
+              <Link href="/login">
+                <span className="underline font-bold">Sign In here</span>
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
       </div>
     </>
   );
