42e6feda8f5d703c6c25b39a73d2af07106725b7
mraihanhatim <raihan04042001@gmail.com>
2024-10-18 15:11:43 +0700

    create handleLogin

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -15,10 +15,21 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
 export default function LoginPage() {
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch("/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: e.currentTarget.email.value,
+        password: e.currentTarget.password.value,
+      }),
+    });
+  };
+
   return (
     <>
       <div className="flex h-screen justify-center items-center p-4">
-        <form>
+        <form onSubmit={(e) => handleLogin(e)}>
           <Card className="w-[450px] shadow-xl">
             <CardHeader>
               <CardTitle className="text-3xl font-bold">Login</CardTitle>
