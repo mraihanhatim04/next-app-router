008066e263a1169bd35cdc2525933ddf02324ef9
mraihanhatim <raihan04042001@gmail.com>
2024-10-18 11:25:53 +0700

    disable the navbar when on the login / register page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
+"use client";
 import "./globals.css";
 import MyNavbar from "./navbar";
 
+import { usePathname } from "next/navigation";
+const disableNavbar = ["/login", "/register"];
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <body>
-        <MyNavbar />
+        {!disableNavbar.includes(pathname) && <MyNavbar />}
+
         {children}
       </body>
     </html>
