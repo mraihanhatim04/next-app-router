37082f25ef520c1d88cee77c5e123662bec12c22
mraihanhatim <raihan04042001@gmail.com>
2024-10-19 13:48:29 +0700

    edit api product

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,24 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
+import apple from "images/apple.png";
+import asus from "images/asus.png";
+import lg from "images/lg.png";
 
 const data = [
   {
     id: 1,
-    name: "Front-end developer",
-    stack: "VueJS, React, Next.js, Shadcn, TailwindCSS",
-    budget: 15000000,
+    title:
+      "Apple iMac (24 inci, M3, 2023) 8C CPU, 8C GPU, 8GB RAM, 256GB, 2 Port, Silver",
+    image: apple,
+    price: 150,
   },
   {
     id: 2,
-    name: "Back-end developer",
-    stack: "Laravel, ExpressJs, NodeJs, MongoDB, Aws",
-    budget: 15000000,
+    title: "LG LED Monitor 24MR400-B 24 FHD IPS 100Hz - FreeSync",
+    image: asus,
+    price: 298,
   },
   {
     id: 3,
-    name: "UI/UX Designer",
-    stack:
-      "Figma, AdobeXD, Sketch, Adobe Photoshop, Blender, Adobe Illustrator",
-    budget: 8000000,
+    title:
+      "ASUS ProArt Display PA278QV Professional Monitor - 27-inch, IPS, WQHD (2560 x 1440), 100% sRGB",
+    image: lg,
+    price: 327,
   },
 ];
 export async function GET(request: NextRequest) {
